1b69cb3a46c27e2a61d75890fb479bfebb2e547b
Abhinay19423 <148855595+Abhinay19423@users.noreply.github.com>
2024-08-05 12:51:45 +0530

    Update TwoSum.go

--- TwoSum.go
+++ TwoSum.go
@@ -1,14 +1,33 @@
-func twoSum(nums []int, target int) []int {
-	a := make(map[int]int)
-    for i:= 0; i<len(nums); i++{
-        temp := target - nums[i]                
-        if val, isTrue := a[temp]; isTrue{
-            return []int{val, i}
-        }
-        a[nums[i]] = i
-    }
-    return []int{}
+package main
 
+import "fmt"
+
+func twoSum(nums [3]int, target int) []int {
+
+	a := make(map[int]int)
+	for i:= 0; i<len(nums); i++{
+		temp := target - nums[i]
+		val, ok := a[temp]
+		if ok{
+			return []int{val, i}
+		}
+		a[nums[i]] = i
+		// fmt.Println(a)
+	}
+	return []int{}
+}
+
+func main(){
+	var a [3]int
+	// fmt.Println("emp ", a)
+	for i:= 0; i<len(a); i+= 1{
+		fmt.Scanf("%v", &a[i])
+	}
+
+	// fmt.Println("enter the target ")
+	var tar int
+	fmt.Scanf("%d", &tar)
+	fmt.Println(twoSum(a, tar))
 }
 
 
